af8e2b9d6749a88c7b8b4b4aadbb3c3c017b6bd2
debrevitatevitae <giotb92@gmail.com>
2022-12-24 18:27:28 +0100

    project_directories.py: add

---
+++ project_directories.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+data_dir = MAIN_DIR + '/data/'
+raw_data_dir = data_dir + 'data_raw/'
+results_dir = MAIN_DIR + '/results/'
