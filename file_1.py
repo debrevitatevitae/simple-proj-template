95eed882d9e0db3ffbc0623982ed1252104a96ff
debrevitatevitae <giotb92@gmail.com>
2022-12-24 18:25:18 +0100

    file_1.py: add
    This is a dummy file, to be edited for real projects

---
+++ file_1.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def my_sum(a:float, b:float) -> float:
+	return a + b
+
+
+if __name__ == '__main__':
+
+	print(my_sum(3, 5))
