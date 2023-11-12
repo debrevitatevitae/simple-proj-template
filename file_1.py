fe3ac1a426936a8d348a2e0acb83d6144b26a906
debrevitatevitae <giotb92@gmail.com>
2023-11-12 08:53:50 +0100

    file_1: rm hardcoded path adaptation

--- file_1.py
+++ file_1.py
@@ -1,18 +1,12 @@
-import os
-import sys
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def my_sum(a:float, b:float) -> float:
-	return a + b
+def my_sum(a: float, b: float) -> float:
+    return a + b
 
 
 if __name__ == '__main__':
-	np.random.seed(0)
-	print(my_sum(3, 5))
+    np.random.seed(0)
+    print(my_sum(3, 5))
